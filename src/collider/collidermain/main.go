fe8d5c35e1db5229e3275a4ac54bdce26b21a31d
Tsukasa Setoguchi <tsukasa.setoguchi@gmail.com>
2020-01-30 22:08:59 +0900

    colliderを手元で起動する

--- src/collider/collidermain/main.go
+++ src/collider/collidermain/main.go
@@ -11,9 +11,9 @@ import (
 	"log"
 )
 
-var tls = flag.Bool("tls", true, "whether TLS is used")
-var port = flag.Int("port", 443, "The TCP port that the server listens on")
-var roomSrv = flag.String("room-server", "https://appr.tc", "The origin of the room server")
+var tls = flag.Bool("tls", false, "whether TLS is used")
+var port = flag.Int("port", 8089, "The TCP port that the server listens on")
+var roomSrv = flag.String("room-server", "http://localhost", "The origin of the room server")
 
 func main() {
 	flag.Parse()
